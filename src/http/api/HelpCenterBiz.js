5e0ea428369b1d000a28eb5710acb3eec7307875
yudanjing <eoin@live.in>
2020-07-15 18:26:08 +0800

    Fix: 接口地址换了

--- src/http/api/HelpCenterBiz.js
+++ src/http/api/HelpCenterBiz.js
@@ -1,5 +1,4 @@
 import axios from '../axios';
-import Util from '@/utils/util';
 import {
   rootDomain
 } from '@/global/constants';
@@ -9,7 +8,7 @@ export class HelpCenterBiz {
    * 获取影集信息
    */
   static getHelpInfo(params = {}) {
-    return axios.post(`${rootDomain}/DingTalkSupportPc/GetSupportContent`, params).then(
+    return axios.post(`${rootDomain}/H5/GetSupportContent`, params).then(
       res => {
         return Promise.resolve(res);
       },
